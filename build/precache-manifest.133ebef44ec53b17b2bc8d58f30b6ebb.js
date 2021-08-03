cb8c92e0e23705fdb8ae10a4701f5c1ed1ea603d
Re3z <ghiz.k.dev@gmail.com>
2021-08-03 04:05:50 +0200

    Conclusion done. 'npm run build' to build the project, 'npm i -g serve' to install globally serve : local server, 'serve -s build' to launch

---
+++ build/precache-manifest.133ebef44ec53b17b2bc8d58f30b6ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d8e1f6c4996b8927606843ebdbe6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec6b7d5ff55ba84c67b",
+    "url": "/static/css/2.0cf305c2.chunk.css"
+  },
+  {
+    "revision": "99ed48d884a3765a3a28",
+    "url": "/static/css/main.0d5c3e97.chunk.css"
+  },
+  {
+    "revision": "2ec6b7d5ff55ba84c67b",
+    "url": "/static/js/2.2f75d227.chunk.js"
+  },
+  {
+    "revision": "99ed48d884a3765a3a28",
+    "url": "/static/js/main.46f57f3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
